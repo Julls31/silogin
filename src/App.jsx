6ca262433c8d25a8c7655ffa5b5bf03fd274e4bb
Julls31 <jullyandrefazry@gmail.com>
2025-05-15 17:32:20 +0700

    fix preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,37 @@
 // src/App.jsx
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./assets/layout/Layout";
-import Home from "./assets/pages/Main/Home";
-import Privacy from "./assets/pages/Privacy";
-import TermAndCondition from "./assets/pages/TermAndCondition";
-import Calculator from "./assets/components/Misc/Calculator";
-import GuideUser from "./assets/components/Guides/GuideUser";
-import GuideVendor from "./assets/components/Guides/GuideVendor";
-import FAQUser from "./assets/components/FAQ/FAQUser";
-import FAQVendor from "./assets/components/FAQ/FAQVendor";
-import NotFound from "./assets/components/Misc/404NotFound";
 import Preloader from "./assets/components/Misc/Preloader";
 
+const Home = lazy(() => import("./assets/pages/Main/Home"));
+const Privacy = lazy(() => import("./assets/pages/Privacy"));
+const TermAndCondition = lazy(() => import("./assets/pages/TermAndCondition"));
+const Calculator = lazy(() => import("./assets/components/Misc/Calculator"));
+const GuideUser = lazy(() => import("./assets/components/Guides/GuideUser"));
+const GuideVendor = lazy(() => import("./assets/components/Guides/GuideVendor"));
+const FAQUser = lazy(() => import("./assets/components/FAQ/FAQUser"));
+const FAQVendor = lazy(() => import("./assets/components/FAQ/FAQVendor"));
+const NotFound = lazy(() => import("./assets/components/Misc/404NotFound"));
+
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoaded(true);
-    }, 500); // Delay for the initial load (ms)
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!isLoaded) {
-    return <Preloader />;
-  }
-
   return (
-     <div className={`scroll-smooth bg-base-100 ${isLoaded ? "fade-in" : ""}`}>
-      <Routes>
-        <Route path="/login" />
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/kebijakan-privasi" element={<Layout><Privacy /></Layout>} />
-        <Route path="/syarat-ketentuan" element={<Layout><TermAndCondition /></Layout>} />
-        <Route path="/kalkulatorpremi" element={<Layout><Calculator /></Layout>} />
-        <Route path="/panduan-pembeli" element={<Layout><GuideUser /></Layout>} />
-        <Route path="/panduan-penyedia" element={<Layout><GuideVendor /></Layout>} />
-        <Route path="/faq-pembeli" element={<Layout><FAQUser /></Layout>} />
-        <Route path="/faq-penyedia" element={<Layout><FAQVendor /></Layout>} />
-        <Route path="*" element={<Layout><NotFound /></Layout>} />
-      </Routes>
-    </div>
+    <Suspense fallback={<Preloader />}>
+      <div className="scroll-smooth bg-base-100 fade-in">
+        <Routes>
+          <Route path="/login" />
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/kebijakan-privasi" element={<Layout><Privacy /></Layout>} />
+          <Route path="/syarat-ketentuan" element={<Layout><TermAndCondition /></Layout>} />
+          <Route path="/kalkulatorpremi" element={<Layout><Calculator /></Layout>} />
+          <Route path="/panduan-pembeli" element={<Layout><GuideUser /></Layout>} />
+          <Route path="/panduan-penyedia" element={<Layout><GuideVendor /></Layout>} />
+          <Route path="/faq-pembeli" element={<Layout><FAQUser /></Layout>} />
+          <Route path="/faq-penyedia" element={<Layout><FAQVendor /></Layout>} />
+          <Route path="*" element={<Layout><NotFound /></Layout>} />
+        </Routes>
+      </div>
+    </Suspense>
   );
 }
 
--- src/assets/components/Misc/Preloader.jsx
+++ src/assets/components/Misc/Preloader.jsx
@@ -6,12 +6,11 @@ function Preloader() {
     <div className="fixed inset-0 bg-white z-50 flex items-center justify-center">
       <div className="flex items-center space-x-2">
         <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-red-600"></div>
-              <div>
-                  <img src="/image/logo1.png" />
-              </div>
+        <div>
+          <img src="/image/logo1.png" />
+        </div>
       </div>
     </div>
-    
   );
 }
 
